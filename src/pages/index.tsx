5cb92d51b9b955a4d4ec2db9b4e096329df8663f
Sheridan Shabani <sheridan.shabani@outlook.fr>
2023-01-22 00:03:35 +0100

    add logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
+import Image from "next/image";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,9 +11,14 @@ export default function Home() {
         <title>Sheridan Shabani - Portfolio</title>
       </Head>
       <main>
-        <h1 className={"text-red-500"}>
-          Sheridan Shabani
-        </h1>
+          <div className={"flex"}>
+              <Image src={"/logo.png"} alt={"logo"}
+                     width={50} height={50}
+                     priority/>
+              <h1 className={"text-red-500"}>
+                  Sheridan Shabani
+              </h1>
+          </div>
       </main>
     </>
   )
