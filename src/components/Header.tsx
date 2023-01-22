39e43c6200ba290a89eb0898b54fbff23b03d447
Sheridan Shabani <sheridan.shabani@outlook.fr>
2023-01-22 13:42:23 +0100

    Header.tsx partially finish

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,27 @@
 import React from "react"
 import Image from "next/image";
+import {motion} from "framer-motion";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSun} from "@fortawesome/free-solid-svg-icons";
 
 export const Header = () => {
     return (
-        <div className={"flex px-4 content-between space-y-2"}>
-            <div className={"flex"}>
+        <header className={"sticky top-0 flex items-start justify-between items-center w-full px-10 pt-4"}>
+            <motion.div
+                initial={{
+                    x: -500,
+                    opacity: 0,
+                    scale: 0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5,
+                }}
+                className={"flex flex-row items-center flex-1 justify-start space-x-3"}>
                 <Image src={"/logo.png"} alt={"logo"}
                        width={50} height={50}
                        priority/>
@@ -12,17 +29,50 @@ export const Header = () => {
                     Sheridan
                     SHABANI
                 </h1>
-            </div>
-            <div>
-                <nav className={"space-x-5"}>
-                    <a>HOME</a>
+            </motion.div>
+            <motion.div
+                initial={{
+                    y: -500,
+                    opacity: 0,
+                    scale: 0.5
+                }}
+                animate={{
+                    y: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5,
+                }}
+                className={"flex flex-row items-center flex-1 justify-center"}>
+                <nav className={"flex flex-row items-center space-x-3"}>
+                    <a className={""}>HOME</a>
                     <a>ABOUT</a>
                     <a>PORTFOLIO</a>
                     <a>RESUME</a>
                     <a>CONTACT</a>
                 </nav>
-            </div>
-        </div>
+            </motion.div>
+            <motion.div
+                initial={{
+                    x: 500,
+                    opacity: 0,
+                    scale: 0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5,
+                }}
+                className={"flex flex-1 justify-end space-x-3 items-center"}>
+                <a>FRA</a>
+                <a>ENG</a>
+                <FontAwesomeIcon className={"px-10"} size={"lg"} icon={faSun} beat/>
+            </motion.div>
+        </header>
 
     )
 }
