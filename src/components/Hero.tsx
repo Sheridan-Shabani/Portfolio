83e96d07ce5bb7bf0e28a23889bb922a3dbcedd4
Sheridan Shabani <sheridan.shabani@outlook.fr>
2023-01-23 18:13:09 +0100

    add priority img and reduce repulse in background

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,7 +18,7 @@ export const Hero = () => {
         <div className={"h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden"}>
             <Image className={"relative rounded-full mx-auto h-32 w-32 object-cover"} src={"/pictureB.png"} alt={"My picture"}
                    width={80}
-            height={50}/>
+            height={50} priority/>
             <div className={"z-20"}>
                 <h3 className={"text-sm uppercase text-[#0075af] pb-2 tracking-[15px]"}>System Engineer</h3>
                 <h2 className={"text-5xl lg:text-6xl font-semibold px-10"}>
--- src/components/ParticlesBackground.tsx
+++ src/components/ParticlesBackground.tsx
@@ -40,7 +40,7 @@ const ParticlesBackground = () => {
                                    quantity: 4,
                                },
                                repulse: {
-                                   distance: 200,
+                                   distance: 50,
                                    duration: 0.4,
                                },
                            },
