855c17e8388818fc1aaaac815bcc6f8baaebb753
Sheridan Shabani <sheridan.shabani@outlook.fr>
2023-01-26 01:03:15 +0100

    start responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,13 @@ import React from "react"
 import Image from "next/image";
 import {motion} from "framer-motion";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faSun} from "@fortawesome/free-solid-svg-icons";
+import {faSun, faBars, faXmark} from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 
 export const Header = () => {
+    const [isNavOpen, setIsNavOpen] = React.useState(false);
     return (
-        <header className={"sticky top-0 flex items-start justify-between items-center w-full px-10 pt-4"}>
+        <header className={"sticky top-0 flex items-start justify-between items-center w-full px-10 pt-4 z-100"}>
             <motion.div
                 initial={{
                     x: -500,
@@ -45,7 +46,8 @@ export const Header = () => {
                 transition={{
                     duration: 1.5,
                 }}
-                className={"flex flex-row items-center flex-1 justify-center"}>
+                className={"hidden lg:flex flex-row items-center flex-1 justify-center"}>
+
                 <nav className={"flex flex-row items-center space-x-3"}>
                     <Link href={"#about"}>
                         <button className={"navButton"}>ABOUT</button>
@@ -81,10 +83,35 @@ export const Header = () => {
                 transition={{
                     duration: 1.5,
                 }}
-                className={"flex flex-1 justify-end space-x-3 items-center"}>
+                className={"hidden lg:flex flex-1 justify-end space-x-3 items-center"}>
                 <button>FRA</button>
                 <button>ENG</button>
                 <button><FontAwesomeIcon className={"px-10"} size={"lg"} icon={faSun} beat/></button>
+            </motion.div>
+            <motion.div
+                initial={{
+                    x: 500,
+                    opacity: 0,
+                    scale: 0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5,
+                }}
+                className={"flex lg:hidden flex-1 justify-end"}>
+                <button onClick={() => setIsNavOpen((prev) => !prev)}>
+                    <FontAwesomeIcon icon={faBars} size={"lg"} fade/>
+                </button>
+                <div className={isNavOpen ? "showMenu" : "hideMenu"}>
+                    <button className={"absolute top-0 right-0 px-8 py-8"}
+                    onClick={() => setIsNavOpen(false)}>
+                        <FontAwesomeIcon icon={faXmark} size={"lg"} fade/>
+                    </button>
+                </div>
             </motion.div>
         </header>
 
