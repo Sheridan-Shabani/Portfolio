41daac12102f9ffdc21c74f40065bacc0ef6eb53
Sheridan Shabani <sheridan.shabani@outlook.fr>
2023-01-27 20:13:56 +0100

    start ExperienceCard.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import {motion} from "framer-motion";
+import {ExperienceCard} from "@/components/ExperienceCard";
 
 export const Experience = () => {
     return (
-        <div className={"h-screen"}>
-
-        </div>
+        <motion.div
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1}}
+            transition={{duration: 2}}
+            className={"h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"}>
+            <h3 className={"absolute top-24 uppercase text-[#0075af] tracking-[20px] text-2xl"}>
+                EXPERIENCE
+            </h3>
+            <div className={"w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory"}>
+                <ExperienceCard/>
+                <ExperienceCard/>
+                <ExperienceCard/>
+                <ExperienceCard/>
+            </div>
+        </motion.div>
     )
 }
